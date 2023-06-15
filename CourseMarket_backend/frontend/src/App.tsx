689ff7d7f929e5dba2d3552998dd01842bc7b381
Alexey-97 <nasretdinov_97@bk.ru>
2023-06-15 20:05:04 +0700

    first commit

---
+++ CourseMarket_backend/frontend/src/App.tsx
@@ -0,0 +1,49 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Registration } from "./pages/Registration/Registration.jsx";
+import { Login } from "./pages/Login/Login.jsx";
+import { ResetPassword } from "./pages/ResetPwd/ResetPwd.jsx";
+import { ForgotPassword } from "./pages/ForgotPwd/ForgotPwd.jsx";
+import { DashBoard } from './pages/DashBoard/DashBoard.jsx';
+import './App.scss'
+import {MailReset} from './pages/MailReset/MailReset.jsx'
+import './assets/style/reset.css';
+
+
+function App() {
+
+  return (
+    <>
+      <Router>
+        <Routes>
+          
+          <Route path="/:uid/:token/" element={<Login />} />
+          {/* логинизация по ссылке из почты */}
+          
+          <Route path="/" element={<Login />} />
+          {/*стартовая страница */}
+{/*           
+          <Route path="/resetPassword/:uid:/:token" element={<ResetPassword />} />
+          // наверное такой путь  */}
+
+          <Route path="/registration" element={<Registration />} />
+
+          <Route path="/forgotPwd" element={<ForgotPassword />} />
+          {/* если забыл пароль */}
+
+          <Route path="/password/:uid/:token/" element={<ResetPassword />} />
+          {/* переходим по ссылке из почты с токеном и паролем для сброса */}
+
+          <Route path="/dashboard" element={<DashBoard />} />
+          {/* добро поджаловать к нам на огонек */}
+
+          <Route path="/mailreset" element={< MailReset />} />
+          {/* вам на почту пришла ссылка для сброса пароля */}
+          
+          
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default App
---
+++ CourseMarket_backend/frontend/src/pages/ResetPwd/ResetPwd.jsx
@@ -0,0 +1,101 @@
+import { Link, useNavigate } from "react-router-dom";
+import style from "./style.module.scss";
+import RegistrationItem from "../../components/Form/RegistrationItem";
+import Button from "../../components/Button/Button";
+import { useForm } from "react-hook-form";
+
+export const ResetPassword = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  //в ссылку вставить id и токен
+  const navigate = useNavigate();
+
+  const onSubmit = async (data) => {
+    const { email } = data;
+    const url = "http://localhost:8000/password/reset/${uid}/${token}/";
+    try {
+      await axios
+        .post(url, {
+          email: email,
+        })
+        .then((res) => {
+          if (res.status === 200) {
+            navigate("/reset");
+          }
+        });
+    } catch (err) {
+      console.warn("ошибка", err);
+    }
+  };
+
+  const checkPassword = () => {
+    const password = document.getElementById("password").value;
+    const confirmpswd = document.getElementById("confirmpswd").value;
+    return password === confirmpswd || false;
+  };
+
+  return (
+    <div className={style.reset}>
+      <section className={style.reset__container}>
+        <h1 className={style.reset__title}>
+          Res
+          <span className={style.reset__title_green}>et</span>
+        </h1>
+        <form
+          action="POST"
+          className={style.reset__registration}
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <div className={style.reset__list}>
+            <RegistrationItem title="новый пароль" error={errors.password}>
+              <input
+                id="password"
+                type="password"
+                {...register("new_password", {
+                  pattern: /^[a-z0-9!?]{8,}$/,
+                  required: true,
+                  validate: checkPassword,
+                })}
+              />
+              {errors.password?.type === "required" && (
+                <span>Введите пароль</span>
+              )}
+              {errors.password?.type === "minLength" && (
+                <span>min Length 9 characters</span>
+              )}
+            </RegistrationItem>
+          </div>
+          <div className={style.reset__list}>
+            <RegistrationItem
+              title="подтверждение пароля"
+              error={errors.re_password}
+            >
+              <input
+                id="confirmpswd"
+                type="password"
+                {...register("re_password", {
+                  pattern: /^[a-z0-9!?]{8,}$/,
+                  required: true,
+                  validate: checkPassword,
+                })}
+              />
+              {errors.re_password?.type === "required" && (
+                <span role="alert">Повторите пароль для подтверждения</span>
+              )}
+            </RegistrationItem>
+          </div>
+        </form>
+        <div className={style.reset__button}>
+          <Button text="Reset Password " type="submit" />
+        </div>
+        <div className={style.links}>
+          <Link to="/">Go Home</Link>
+        </div>
+      </section>
+    </div>
+  );
+};
